dd546397bdd9a23c16e6904af66e31500ba0a3df
roarinzebra <ros.intelligente@gmail.com>
2025-01-20 11:40:31 +0100

    fix build errors

--- packages/certification/tsup.config.ts
+++ packages/certification/tsup.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: {
+    entry: './src/index.ts',
+    resolve: true
+  },
   clean: true,
   external: [
     '@greatforai/core',
