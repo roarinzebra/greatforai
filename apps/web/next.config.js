cdd3e887e6d5f3b7483ca1b53e8c61f78618b0a2
roarinzebra <ros.intelligente@gmail.com>
2025-01-20 13:59:53 +0100

    created next.config.js

---
+++ apps/web/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: 'dist',
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig; 
