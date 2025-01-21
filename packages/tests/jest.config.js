375fe37d7d01081131819d9d50c27aa6d3b36b17
roarinzebra <ros.intelligente@gmail.com>
2025-01-21 11:54:16 +0100

    added jest and artifact configuration

---
+++ packages/tests/jest.config.js
@@ -0,0 +1,18 @@
+/* eslint-env node */
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+};
+
+export default config; 
---
+++ packages/tests/src/index.test.ts
@@ -0,0 +1,5 @@
+describe('Basic test suite', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+}); 
